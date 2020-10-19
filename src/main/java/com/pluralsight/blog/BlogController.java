30263fbc84a28e7ec534f33bfe3921ebedeebcb8
Gregory Zentkovich <gzentkovich@gmail.com>
2020-10-18 19:30:19 -1000

    add ModelMap with message

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,13 +1,16 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
 
     @RequestMapping("/")
-    public String listPosts(){
+    public String listPosts(ModelMap modelMap){
+        String message = "Blog Post 1";
+        modelMap.put("title", message);
         return "home";
     }
 }
